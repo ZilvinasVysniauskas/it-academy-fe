a04e76be6e390b9f99b522311919a66eb9ee85be
ZilvinasVysniauskas <zilvinas.Vysniauaskas99@gmail.com>
2022-04-06 21:35:38 +0300

    x

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/it-academyfe'));
+app.use(express.static(__dirname + '/dist/it-academy-fe'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/it-academyfe/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/it-academy-fe/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
