70db94487c34ae4bc4996389dc85a506925c5310
ZilvinasVysniauskas <zilvinas.Vysniauaskas99@gmail.com>
2022-04-06 20:39:28 +0300

    x

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,13 +30,13 @@ describe('AppComponent', () => {
   it(`should have as title 'it-academy-fe'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    // expect(app.title).toEqual('it-academy-fe');
+    expect(app.title).toEqual('it-academy-fe');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('it-academy-fe app is running!');
-  });
+  // it('should render title', () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.nativeElement as HTMLElement;
+  //   expect(compiled.querySelector('.content span')?.textContent).toContain('it-academy-fe app is running!');
+  // });
 });
