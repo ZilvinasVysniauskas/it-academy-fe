9270ef39bea06d7ccfc2c37bf4c292b48a1319e6
ZilvinasVysniauskas <zilvinas.Vysniauaskas99@gmail.com>
2022-04-06 21:29:39 +0300

    x

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HerokuTestComponent} from "./heroku-test/heroku-test.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HerokuTestComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
