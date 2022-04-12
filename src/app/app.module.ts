c30629ab0483f9c04f061bdee12be0bfd1906e30
ZilvinasVysniauskas <zilvinas.Vysniauaskas99@gmail.com>
2022-04-12 17:39:23 +0300

    added successful reservation message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { HerokuTestComponent } from './heroku-test/heroku-test.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule } from '@angular/material/input';
 import { LoginComponent } from './components/login/login.component';
@@ -40,7 +39,6 @@ export const MY_DATE_FORMATS = {
 @NgModule({
   declarations: [
     AppComponent,
-    HerokuTestComponent,
     LoginComponent,
     CalendarComponent,
     HeaderComponent,
