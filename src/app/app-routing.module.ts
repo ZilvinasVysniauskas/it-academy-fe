28f4422b35a71ac09648a5db53fc0723294c3afa
ZilvinasVysniauskas <zilvinas.Vysniauaskas99@gmail.com>
2022-04-12 17:39:59 +0300

    added successful reservation message

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {HerokuTestComponent} from "./heroku-test/heroku-test.component";
 import { HomeComponent } from './components/home/home.component';
 import { ReservationsComponent } from './components/reservations/reservations.component';
 import { LoginComponent } from './components/login/login.component';
