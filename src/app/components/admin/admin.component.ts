0d93c55cc42d6ef54163e6e798bbbbdaf1816128
ZilvinasVysniauskas <zilvinas.Vysniauaskas99@gmail.com>
2022-04-13 08:33:03 +0300

    admin-page

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {User} from "../../interfaces/user";
 import {AdminPageService} from "../../service/admin/admin-page.service";
-import {FormControl, FormGroup} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {map, startWith} from "rxjs/operators";
 import {combineLatest, Observable, of} from "rxjs";
 
@@ -15,41 +15,24 @@ export class AdminComponent implements OnInit {
 
   maxLength = 20;
 
-
-  searchUserForm!: FormGroup;
-
-  get expUserId() {
-    return this.searchUserForm.get('userId')
-  }
-
-
-
-  get expLastName() {
-    return this.searchUserForm.get('lastName')
-  }
-
-  get expFirstName() {
-    return this.searchUserForm.get('firstName')
-  }
+  public readonly searchControl: FormControl;
+  public readonly users$: Observable<User[]>;
 
   constructor(private adminService: AdminPageService) {
-    this.searchUserForm = new FormGroup({
-        userId: new FormControl(''),
-        firstName: new FormControl(''),
-        lastName: new FormControl('')
-      }
+    this.searchControl = new FormControl('', [Validators.required]);
+
+    this.users$ = combineLatest([
+      this.searchControl.valueChanges.pipe(startWith('')),
+      this.adminService.fetchAllUsers(),
+    ]).pipe(
+      map(([ search, allUsers ]: [ string, User[] ]): User[] => allUsers.filter(
+        (user: User): boolean => user.firstName.includes(search),
+      )),
     );
   }
   users!: User[];
 
   ngOnInit(): void {
-    this.adminService.fetchAllUsers().subscribe(u => {
-      this.users = u;
-    });
-    this.expFirstName!.valueChanges.pipe(
-      startWith(''),
-      map(value => this.users.filter(user => user.firstName.includes(value)))
-    ).subscribe(u => this.users = u)
   }
 
 
