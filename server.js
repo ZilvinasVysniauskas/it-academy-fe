eec202a9493c671c664f0f6ce0d1fca6280c0cb4
ZilvinasVysniauskas <zilvinas.Vysniauaskas99@gmail.com>
2022-04-24 22:42:55 +0300

    x

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/heroku-angular-fe'));
+app.use(express.static('/dist/heroku-angular-fe'));
 
 app.get('/*', function(req,res) {
 
