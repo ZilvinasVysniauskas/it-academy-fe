3773a7950b39660daf825b05d6b9fab46af1ff01
ZilvinasVysniauskas <zilvinas.Vysniauaskas99@gmail.com>
2022-04-25 10:58:56 +0300

    x

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 const port = process.env.PORT || 3001;
 
-app.use(express.static(__dirname + '/dist/little-rent'));
+app.use(express.static(__dirname + '/dist/it-academy-fe'));
 
 app.get('/*', (req, res) => res.sendFile(path.join(__dirname)));
 
