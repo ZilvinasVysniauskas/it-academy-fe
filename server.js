ca9211036a922ed258d8db31799d22a500b244a9
ZilvinasVysniauskas <zilvinas.Vysniauaskas99@gmail.com>
2022-04-25 15:22:02 +0300

    x

--- server.js
+++ server.js
@@ -1,12 +1,35 @@
-//Install express server
 const express = require('express');
+const http = require('http');
 const path = require('path');
+
 const app = express();
-// Serve only the static files form the dist directory
-app.use(express.static('/it-academy-fe'));
+
+const port = process.env.PORT || 3001;
+
+
+app.use(express.static(__dirname + '/dist/it-academy-fe/index.html'));
 
 app.get('/*', (req, res) =>
   res.sendFile('index.html', {root: '/it-academy-fe/'}),
 );
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname)));
+
+const server = http.createServer(app);
+
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+        ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+// Instruct the app
+// to use the forceSSL
+// middleware
+app.use(forceSSL());
+
+server.listen(port, () => console.log(`App running on: http://localhost:${port}`));
